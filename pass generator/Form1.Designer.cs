a4878efd2b2145221dacd7e8117fb778476c4b81
Vojin Golubovic <36790210+fl1k@users.noreply.github.com>
2018-06-28 20:30:28 +0200

    published

---
+++ pass generator/Form1.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace pass_generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.ForeColor = System.Drawing.Color.SeaShell;
+            this.button1.Location = new System.Drawing.Point(349, 124);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Black;
+            this.button2.ForeColor = System.Drawing.Color.SeaShell;
+            this.button2.Location = new System.Drawing.Point(349, 256);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 40);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Add";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Snow;
+            this.label1.Location = new System.Drawing.Point(43, 138);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Password Length: ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.ControlText;
+            this.textBox2.ForeColor = System.Drawing.Color.LimeGreen;
+            this.textBox2.Location = new System.Drawing.Point(44, 199);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(262, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox2_MouseClick);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Black;
+            this.button4.ForeColor = System.Drawing.Color.SeaShell;
+            this.button4.Location = new System.Drawing.Point(68, 308);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(112, 40);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Exit";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.ControlText;
+            this.textBox3.ForeColor = System.Drawing.Color.LimeGreen;
+            this.textBox3.Location = new System.Drawing.Point(46, 267);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(262, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Snow;
+            this.label3.Location = new System.Drawing.Point(143, 178);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Password";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Snow;
+            this.label4.Location = new System.Drawing.Point(120, 246);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Reminder(optional)";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Black;
+            this.button3.ForeColor = System.Drawing.Color.SeaShell;
+            this.button3.Location = new System.Drawing.Point(349, 188);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 40);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Copy";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.Snow;
+            this.label5.Location = new System.Drawing.Point(120, 71);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Username(optional)";
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.ControlText;
+            this.textBox4.ForeColor = System.Drawing.Color.LimeGreen;
+            this.textBox4.Location = new System.Drawing.Point(44, 92);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(262, 20);
+            this.textBox4.TabIndex = 13;
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.SystemColors.ControlText;
+            this.textBox5.ForeColor = System.Drawing.Color.LimeGreen;
+            this.textBox5.Location = new System.Drawing.Point(26, 22);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(540, 20);
+            this.textBox5.TabIndex = 23;
+            this.textBox5.Text = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890[];\',.{}|:<>?!@#$%^" +
+    "&*()_+=-~`";
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Checked = true;
+            this.checkBox4.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox4.ForeColor = System.Drawing.Color.Snow;
+            this.checkBox4.Location = new System.Drawing.Point(497, 243);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(52, 17);
+            this.checkBox4.TabIndex = 22;
+            this.checkBox4.Text = "chars";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.check);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Checked = true;
+            this.checkBox3.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox3.ForeColor = System.Drawing.Color.Snow;
+            this.checkBox3.Location = new System.Drawing.Point(497, 220);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(66, 17);
+            this.checkBox3.TabIndex = 21;
+            this.checkBox3.Text = "numbers";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.check);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.ForeColor = System.Drawing.Color.Snow;
+            this.checkBox2.Location = new System.Drawing.Point(497, 197);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(75, 17);
+            this.checkBox2.TabIndex = 20;
+            this.checkBox2.Text = "large caps";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.check);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.ForeColor = System.Drawing.Color.Snow;
+            this.checkBox1.Location = new System.Drawing.Point(497, 174);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(75, 17);
+            this.checkBox1.TabIndex = 19;
+            this.checkBox1.Text = "small caps";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.check);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.numericUpDown1.ForeColor = System.Drawing.Color.Beige;
+            this.numericUpDown1.Location = new System.Drawing.Point(135, 136);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            2048,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(60, 20);
+            this.numericUpDown1.TabIndex = 24;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            32,
+            0,
+            0,
+            0});
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Checked = true;
+            this.checkBox5.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox5.ForeColor = System.Drawing.Color.Snow;
+            this.checkBox5.Location = new System.Drawing.Point(201, 139);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(82, 17);
+            this.checkBox5.TabIndex = 25;
+            this.checkBox5.Text = "unique char";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            this.checkBox5.CheckedChanged += new System.EventHandler(this.check);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(586, 372);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Password Manager / Generator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.CheckBox checkBox5;
+    }
+}
+
---
+++ pass generator/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Windows;
+using System.Security.Cryptography;
+
+
+namespace pass_generator
+{
+    public partial class Form1 : Form
+    {
+        string path = Environment.CurrentDirectory + "/" + "Passwords" + ".html";
+        Random r1 = new Random();
+        int jsid;
+        public Form1()
+        {
+            InitializeComponent();
+            generate();
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine("/////////////////////////////////////////////////////////////////<br>//  File created by fl1k's password generator  //<br>/////////////////////////////////////////////////////////////////<br><br><br>");
+                    sw.WriteLine("<style>\ndiv#pass {\nline-height: 1.4;\n}\n#pass span {\ndisplay: inline-block;\nwidth: 70px;\n}\n </style>\n<script>\nfunction copy(pass_loc){\nvar txt = document.getElementById(pass_loc);\ntxt.select();\ndocument.execCommand(\"Copy\");\n}\n</script>\n<div id=pass>");
+                }
+            }
+            jsid = File.ReadLines(path).Count();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            generate();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            jsid = File.ReadLines(path).Count();
+            if (textBox2.TextLength >= 1)
+            {
+                using (StreamWriter sw = File.AppendText(path))
+                {
+                    sw.WriteLine($"\n\n\n<span>Date:</span> <input type=\"text\" value=\"{DateTime.Now}\"><br>");
+                    sw.WriteLine($"<span>Username:</span> <input type=\"text\" value=\"{textBox4.Text}\"><br>");
+                    sw.WriteLine($"<span>Password:</span> <input type=\"text\" value=\"{textBox2.Text}\" id=\"{jsid}\">");
+                    sw.WriteLine($"<button onclick=copy(\"{jsid}\")>Copy</button><br>");
+                    sw.WriteLine($"<span>Reminder:</span> <input type=\"text\" value=\"{textBox3.Text}\">  <p>--------------------------------------------------------------</p><br>");
+                    MessageBox.Show("Password added to the HTML file.", "Password Manager / Generator", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else
+            {
+                MessageBox.Show("There is no password generated.", "Password Manager / Generator", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void generate()
+        {
+            string rs = string.Empty;
+            char[] array = textBox5.Text.ToCharArray();
+            if (checkBox5.Checked == false)
+            {
+                for (int i = 0; i < numericUpDown1.Value; i++)
+                {
+                    int point = r1.Next(0, textBox5.TextLength - 1);
+                    rs += array.GetValue(point);
+                }
+            }
+            else if (checkBox5.Checked == true)
+            {
+                for (int i = 0; i < numericUpDown1.Value; i++)
+                {
+                    int point = r1.Next(0, textBox5.TextLength);
+                    if (!rs.Contains(array.GetValue(point).ToString()))
+                        rs += array.GetValue(point);
+                    else
+                        i--;
+                }
+            }
+            textBox2.Text = rs;
+        }
+
+        private void check(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            if (checkBox1.Checked && checkBox2.Checked && checkBox3.Checked && checkBox4.Checked)
+                textBox5.Text = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890[];',.{}|:<>?!@#$%^&*()_+=-~`";
+            else if (checkBox1.Checked && checkBox2.Checked && checkBox3.Checked)
+                textBox5.Text = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890";
+            else if (checkBox1.Checked && checkBox2.Checked && checkBox4.Checked)
+                textBox5.Text = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM[];',./{}|:<>?!@#$%^&*()_+=-~`";
+            else if (checkBox1.Checked && checkBox3.Checked && checkBox4.Checked)
+                textBox5.Text = "qwertyuiopasdfghjklzxcvbnm1234567890[];',.{}|:<>?!@#$%^&*()_+=-~`";
+            else if (checkBox2.Checked && checkBox3.Checked && checkBox4.Checked)
+                textBox5.Text = "QWERTYUIOPASDFGHJKLZXCVBNM1234567890[];',.{}|:<>?!@#$%^&*()_+=-~`";
+            else if (checkBox1.Checked && checkBox2.Checked)
+                textBox5.Text = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+            else if (checkBox1.Checked && checkBox3.Checked)
+                textBox5.Text = "qwertyuiopasdfghjklzxcvbnm1234567890";
+            else if (checkBox1.Checked && checkBox4.Checked)
+                textBox5.Text = "qwertyuiopasdfghjklzxcvbnm[];',.{}|:<>?!@#$%^&*()_+=-~`";
+            else if (checkBox2.Checked && checkBox3.Checked)
+                textBox5.Text = "QWERTYUIOPASDFGHJKLZXCVBNM1234567890";
+            else if (checkBox2.Checked && checkBox4.Checked)
+                textBox5.Text = "QWERTYUIOPASDFGHJKLZXCVBNM[];',.{}|:<>?!@#$%^&*()_+=-~`";
+            else if (checkBox3.Checked && checkBox4.Checked)
+                textBox5.Text = "1234567890[];',.{}|:<>?!@#$%^&*()_+=-~`";
+            else if (checkBox1.Checked)
+                textBox5.Text = "qwertyuiopasdfghjklzxcvbnm";
+            else if (checkBox2.Checked)
+                textBox5.Text = "QWERTYUIOPASDFGHJKLZXCVBNM";
+            else if (checkBox3.Checked)
+                textBox5.Text = "1234567890";
+            else if (checkBox4.Checked)
+                textBox5.Text = "[];',.{}|:<>?!@#$%^&*()_+=-~`";
+            else
+            {
+                button1.Enabled = false;
+                textBox5.Text = " ";
+            }
+
+            if (checkBox5.Checked == true)
+                numericUpDown1.Maximum = textBox5.TextLength;
+            else
+                numericUpDown1.Maximum = 2048;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox2.Focus();
+            textBox2.SelectAll();
+            Clipboard.SetText(textBox2.Text);
+        }
+
+        private void textBox2_MouseClick(object sender, MouseEventArgs e)
+        {
+            textBox2.Focus();
+            textBox2.SelectAll();
+            Clipboard.SetText(textBox2.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Button[] buttons = { button1, button2, button3, button4 };
+            TextBox[] txtb = { textBox2, textBox3, textBox4, textBox5 };
+            foreach (Button b in buttons)
+            {
+                b.BackColor = Color.FromArgb(35, 35, 35);
+                b.TabStop = false;
+                b.FlatStyle = FlatStyle.Flat;
+                b.FlatAppearance.BorderSize = 0;
+            }
+
+            foreach (TextBox txt in txtb)
+            {
+                txt.BackColor = Color.FromArgb(35, 35, 35);
+                txt.TabStop = false;
+                txt.BorderStyle = BorderStyle.FixedSingle;
+            }
+            this.numericUpDown1.BackColor = Color.FromArgb(35, 35, 35);
+        }
+    }
+}
